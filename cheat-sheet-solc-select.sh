812da55c63224de384c85184b2346b59ca5b3dde
Remus <127229497+RemusDBD@users.noreply.github.com>
2024-11-22 12:15:47 +0800

    Create cheat-sheet-solc-select.sh

---
+++ cheat-sheet-solc-select.sh
@@ -0,0 +1,13 @@
+# etherumbook - solc-select 
+$solc-select install 0.6.4
+#Installing solc '0.6.4'...
+#Version '0.6.4' installed.
+$solc-select use 0.6.4
+#Switched global version to 0.6.4
+$solc --version
+#solc, the solidity compiler commandline interface
+#Version: 0.6.4+commit.1dca32f3.Linux.g++
+$dir
+#Faucet.sol  workspace.code-workspace
+$solc Faucet.sol 
+#Compiler run successful, no output requested.
