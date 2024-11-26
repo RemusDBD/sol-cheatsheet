406ffd05442d38709d2f378d6bdb57fa8c1cfef8
Remus <127229497+RemusDBD@users.noreply.github.com>
2024-11-26 10:31:49 +0800

    Create cheat-sheet-solc-output.sh

---
+++ cheat-sheet-solc-output.sh
@@ -0,0 +1,7 @@
+# I ran this on vscode terminal
+# Guide followed by : Ethereumbook Ch.13
+
+$ solc --opcodes Example.sol -o opcodes
+$ solc --asm Example.sol -o asm
+$ solc --bin Example.sol -o bin
+$ solc --bin-runtime Example.sol -o --bin-runtime #hex bytecode
